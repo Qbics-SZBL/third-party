64b4f149a5004db24de7be297c06ea0d4f6f0940
Hans Pabst <hans.pabst@intel.com>
2026-06-11 15:15:24 +0200

    Improved supporting BUILD_SHARED_LIBS=OFF

--- scripts/libxsConfig.cmake
+++ scripts/libxsConfig.cmake
@@ -1,27 +1,38 @@
-if(TARGET libxs::libxs)
-  return()
-endif()
-
 get_filename_component(_prefix "${CMAKE_CURRENT_LIST_DIR}/../../.." ABSOLUTE)
 
+set(_libxs_suffixes_save "${CMAKE_FIND_LIBRARY_SUFFIXES}")
+if(DEFINED BUILD_SHARED_LIBS AND NOT BUILD_SHARED_LIBS)
+  set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
+endif()
 find_library(LIBXS_LIBRARY NAMES xs HINTS "${_prefix}/lib" NO_DEFAULT_PATH)
+if(NOT LIBXS_LIBRARY)
+  set(CMAKE_FIND_LIBRARY_SUFFIXES "${_libxs_suffixes_save}")
+  find_library(LIBXS_LIBRARY NAMES xs HINTS "${_prefix}/lib" NO_DEFAULT_PATH)
+endif()
+set(CMAKE_FIND_LIBRARY_SUFFIXES "${_libxs_suffixes_save}")
+unset(_libxs_suffixes_save)
 find_path(LIBXS_INCLUDE_DIR NAMES libxs/libxs.h HINTS "${_prefix}/include" "${_prefix}" NO_DEFAULT_PATH)
 
 if(LIBXS_LIBRARY AND LIBXS_INCLUDE_DIR)
-  find_package(Threads QUIET)
-  set(_incdirs "${LIBXS_INCLUDE_DIR}")
-  if(EXISTS "${LIBXS_INCLUDE_DIR}/libxs/libxs.mod")
-    list(APPEND _incdirs "${LIBXS_INCLUDE_DIR}/libxs")
+  if(NOT TARGET libxs::libxs)
+    find_package(Threads QUIET)
+    set(_incdirs "${LIBXS_INCLUDE_DIR}")
+    if(EXISTS "${LIBXS_INCLUDE_DIR}/libxs/libxs.mod")
+      list(APPEND _incdirs "${LIBXS_INCLUDE_DIR}/libxs")
+    endif()
+    add_library(libxs::libxs UNKNOWN IMPORTED)
+    set_target_properties(libxs::libxs PROPERTIES
+      IMPORTED_LOCATION "${LIBXS_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${_incdirs}")
+    if(TARGET Threads::Threads)
+      set_property(TARGET libxs::libxs APPEND PROPERTY
+        INTERFACE_LINK_LIBRARIES Threads::Threads)
+    endif()
+    unset(_incdirs)
+  else()
+    set_property(TARGET libxs::libxs PROPERTY
+      IMPORTED_LOCATION "${LIBXS_LIBRARY}")
   endif()
-  add_library(libxs::libxs UNKNOWN IMPORTED)
-  set_target_properties(libxs::libxs PROPERTIES
-    IMPORTED_LOCATION "${LIBXS_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${_incdirs}")
-  if(TARGET Threads::Threads)
-    set_property(TARGET libxs::libxs APPEND PROPERTY
-      INTERFACE_LINK_LIBRARIES Threads::Threads)
-  endif()
-  unset(_incdirs)
 endif()
 
 include(FindPackageHandleStandardArgs)
